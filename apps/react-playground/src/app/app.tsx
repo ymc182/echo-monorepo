20d30b16f32b9d6aba4669a8c1ceff7b448f9482
ymc182 <ymc182@gmail.com>
2023-04-04 14:41:03 +1000

    echo-mono init

--- apps/react-playground/src/app/app.tsx
+++ apps/react-playground/src/app/app.tsx
@@ -1,15 +1,18 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from "./app.module.css";
-
-import NxWelcome from "./nx-welcome";
+import { useNearContext } from '@echo-monorepo/near-provider';
 
 export function App() {
+  const { accountId } = useNearContext();
   return (
-    <>
-      <NxWelcome title="react-playground" />
-
-      <div />
-    </>
+    <div className="bg-gray-900 text-white">
+      <div>Header</div>
+      <div className="flex place-content-center place-items-center h-screen ">
+        Clean
+        <span role="img" aria-label="love-emoji">
+          ❤️
+        </span>
+        React x Tailwindcss {accountId}
+      </div>
+    </div>
   );
 }
 
